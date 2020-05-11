83840f4700417f6234adcd62dd1caaa9fa526528
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-05-11 12:04:55 -0400

    color wheel working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Wheel from './Wheel.js'
+// import GuidanceContainer from './GuidanceContainer.js'
 
 function App() {
   return (
     <div className="App">
       <h1> Welcome to the Wellness Center</h1>
-      {/** TODO: Import and render the Wheel component, provided for you. It does not need any props. */}
+      <Wheel/>
       <hr />
       {/** TODO: Import and render the GuidanceContainer component, provided for you. It does not need any props. */}
     </div>
--- src/Wheel.js
+++ src/Wheel.js
@@ -3,11 +3,14 @@ import { wellnessFactors } from './data';
 import WellnessItem from './WellnessItem';
 import { Pie } from './styles';
 
-
+// console.log(wellnessFactors) //array of objects
+// console.log(WellnessItem) //component needs id, color, title as props, use destructuring assignemnt
 export default class Wheel extends React.Component {
+
     render(){
         return (
             <Pie size={300}>
+                {wellnessFactors.map((factor, index) => <WellnessItem factor={factor} key={index}/>)}
                 {/** TODO: map over your wellness factors and create a WellnessItem for each. 
                  *         check the WellnessItem component to see what it's expecting for props
                  *         Hint: Use your ES6 techniques
